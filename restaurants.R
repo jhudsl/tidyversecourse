77c192fb9dc32e3c69728480d7b1771317d24550
Roger D. Peng <rdpeng@jhu.edu>
2020-06-19 12:17:42 -0400

    Restaurant analysis code

--- restaurants.R
+++ restaurants.R
@@ -4,7 +4,12 @@
 library(tidyverse)
 library(lubridate)
 
-dat <- read_csv("data/Restaurant_Scores_-_LIVES_Standard.csv.gz")
+dat <- read_csv("data/Restaurant_Scores_-_LIVES_Standard.csv.gz") %>%
+        mutate(risk = fct_collapse(risk_category,
+                                   high = "High Risk",
+                                   other_level = "low"),
+               zip = factor(business_postal_code),
+               date = as.Date(mdy_hms(inspection_date)))
 
 ## outcome: risk_category
 
@@ -15,6 +20,25 @@ dat %>%
         coord_fixed()
 
 dat %>%
+        filter(!is.na(violation_description)) %>%
+        select(business_id, inspection_type, risk)
+
+dat %>%
+        filter(!is.na(risk)) %>%
+        group_by(inspection_type) %>%
+        summarize(riskp = mean(risk == "high", na.rm = TRUE)) %>%
+        arrange(desc(riskp))
+
+dat %>%
         mutate(inspection_date = mdy_hms(inspection_date)) %>%
         ggplot(aes(inspection_date, inspection_score)) + 
-        geom_point()
+        geom_point() + 
+        geom_smooth()
+
+fit <- glm(risk ~ date,
+           data = dat, 
+           family = binomial)
+
+tidy(fit) %>%
+        as.data.frame()
+        
