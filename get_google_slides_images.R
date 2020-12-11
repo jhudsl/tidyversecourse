47a2571d41ad6c135d8911dc157d76bbd4b19ac4
Roger D. Peng <rdpeng@jhu.edu>
2020-12-11 11:20:33 -0500

    Write code for replacing URLs

--- get_google_slides_images.R
+++ get_google_slides_images.R
@@ -1,6 +1,7 @@
 ## Download Google Slides Images
 
 library(tidyverse)
+library(stringr)
 
 files <- dir(pattern = glob2rx("0*.Rmd"))
 text <- lapply(files, read_lines) %>%
@@ -19,3 +20,35 @@ for(i in seq_along(infiles)) {
         download.file(infiles[i], outfiles[i])
         Sys.sleep(1)
 }
+
+
+## Edit URLs in Rmd files
+files <- dir(pattern = glob2rx("0*.Rmd"))
+dict <- cbind(url = infiles, newurl = outfiles)
+
+for(i in seq_along(files)) {
+        message(files[i])
+        text <- read_lines(files[i])
+        for(j in seq_len(nrow(dict))) {
+                idx <- grep(dict[j, 1], text, fixed = TRUE)
+                text[idx] <- sub(dict[j, 1], dict[j, 2], text[idx], fixed = TRUE)
+        }
+        write_lines(text, files[i])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
